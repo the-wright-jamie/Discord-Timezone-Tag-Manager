4fe861b1a8bffa5895aef849e9bd40486bc66e61
Yokai・妖怪 <ghostcodedev@gmail.com>
2021-07-20 19:38:21 +0100

    First Commit from Intellij

---
+++ app/src/main/java/com/yokai64/dttm/MainActivity.java
@@ -0,0 +1,20 @@
+package com.yokai64.dttm;
+
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.fragment.app.DialogFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+}
